2132bcdc49e4f4428282134fbc12e971bf6ce33f
otw03 <ypd01476@gmail.com>
2023-03-10 01:45:47 +0900

    JS 복습 12비동기

--- JS/codes/test.js
+++ JS/codes/test.js
@@ -1,23 +1,24 @@
-// 함수의 호이스팅은 함수의 선언문 전에 호출이 가능하게 해줌
-// 함수의 선언문은 선언 이전에도 호출이 가능함
-print();
-
-function print() {
-  console.log('Hello');
+// 클로저를 이용한 방법
+function makeCounter() {
+  let count = 0;
+  function increase() {
+    count++;
+    console.log(count);
+  }
+  return increase;
 }
+const increase = makeCounter();
+increase(); // 1
+increase(); // 2
+increase(); // 3
 
-// 변수(let, const)와 클래스는 선언만 호이스팅이 되고,
-// 초기화는 안됨
-// 초기화 전, 변수에 접근하면 컴파일(빌드) 에러가 발생
-// console.log(hi);
-let hi = 'hi';
-let func1 = function () {};
-
-// const cat = new Cat();
-class Cat {}
-
-let x = 1;
-{
-  console.log(x);
-  let x = 2;
+// 클래스를 이용한 방법
+class Counter {
+  #count = 0;
+  increase() {
+    this.#count++;
+    console.log(this.#count);
+  }
 }
+const counter = new Counter();
+counter.increase(); // 1
